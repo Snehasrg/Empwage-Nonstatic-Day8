f966f158623e077de3a39e97c59b55d619ca43c1
Snehasrg <sghongade642@gmail.com>
2023-01-01 23:20:24 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Nonstatic_Empwage;
+
+namespace NonstaticEmpWage
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation by Non static method");
+
+           
+        }
+    }
+}
