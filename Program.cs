61b9bbabb4debcd44cb09b40f00ff60882ffe553
Snehasrg <sghongade642@gmail.com>
2023-01-03 23:15:40 +0530

    Greeting

--- Program.cs
+++ Program.cs
@@ -7,7 +7,24 @@ namespace NonstaticEmpWage
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation by Non static method");
+           
+            //CheckAttendNonstatic obj = new CheckAttendNonstatic();
+            //obj.checkAttendence();
 
+            // DailyEmpWageNonstatic obj = new DailyEmpWageNonstatic();
+            //obj.CheckWage();
+
+            // ParttimeEmpWageNonstatic obj = new ParttimeEmpWageNonstatic();
+            // obj.CheckWage();
+
+            // SwitchcaseNonstatic obj = new SwitchcaseNonstatic();
+            //obj.CheckWage();
+
+            //EmpMonthWageNonstatic obj = new EmpMonthWageNonstatic();
+            //obj.CheckWage();
+
+            //CondTotalNonstatic obj = new CondTotalNonstatic();
+            //obj.CheckWage();
         }
     }
 }
