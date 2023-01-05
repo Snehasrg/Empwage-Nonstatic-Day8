847e5a48c9e131ab471c9b83c57a43bd3c362dc6
Snehasrg <sghongade642@gmail.com>
2023-01-05 19:17:37 +0530

    Part Time Emp Wage Added

---
+++ ParttimeEmpWageNonstatic.cs
@@ -0,0 +1,39 @@
+﻿namespace Nonstatic_Empwage
+{
+    public class ParttimeEmpWageNonstatic
+    {
+        public void CheckWage()
+        {
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else if (empCheck == PART_TIME)
+            {
+                Console.WriteLine("Employee is present Part time");
+                empHrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is" + empWage);
+
+        }
+
+    }
+}
