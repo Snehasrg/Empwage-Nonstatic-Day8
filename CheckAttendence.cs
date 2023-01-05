1b3000c6b065f49bf6c1a404698d290c9643f10e
Snehasrg <sghongade642@gmail.com>
2023-01-05 19:01:35 +0530

    Checking Attendence

---
+++ CheckAttendence.cs
@@ -0,0 +1,20 @@
+﻿namespace Nonstatic_Empwage
+{
+    public class CheckAttendence
+    {
+        public void check()
+        {
+            int Is_Full_Time = 1;
+            Random R = new Random();
+            int empCheck = R.Next(2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
